12968e38269ef647d5412b7373259eee815a5717
Daniel Hernández Martínez <danihermar00@gmail.com>
2025-07-06 03:52:12 +0200

    Repo reorganization + webapp updated

--- webapp/pages/2_Detailed process.py
+++ webapp/pages/2_Detailed process.py
@@ -7,10 +7,23 @@ import scipy.stats
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 import random
+import os
+import pickle
+
+# Custom files
+from system.energy_calculator import FuzzyController, EnergyCalculator
+from system.hybrid_music_recommender import ALSRecommender, KmeansContentBasedRecommender, HybridRecommender
+from system.two_stage_system import MusicRecommender2Stages
+
+BASE_DIR = os.getcwd()
+RESOURCES_DIR = os.path.join(BASE_DIR, 'resources')
+DATA_DIR = os.path.join(RESOURCES_DIR, 'data')
+MODEL_DIR = os.path.join(RESOURCES_DIR, 'models')
+MATRICES_DIR = os.path.join(RESOURCES_DIR, 'matrices')
+
+# Clear cache
+#st.cache_data.clear()
 
 st.set_page_config(
     page_title="Music recommender",
@@ -18,156 +31,173 @@ st.set_page_config(
     layout="centered"
 )
 
+@st.cache_data
+def load_csv(base_path, file_name):
+    file_path = os.path.join(base_path, file_name)
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path)
+    else:
+        st.error(f"File {file_name} not found in {base_path}")
+        return None
+    
+@st.cache_data
+def load_cluster_mapping(base_path, file_name):
+    file_path = os.path.join(base_path, file_name)
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path).set_index('track_id').iloc[:, 0]
+    else:
+        st.error(f"File {file_name} not found in {base_path}")
+        return None
+    
+
+@st.cache_data
+def load_numpy_data(base_path, file_name):
+    file_path = os.path.join(base_path, file_name)
+    if os.path.exists(file_path):
+        return np.load(file_path)
+    else:
+        st.error(f"File {file_name} not found in {base_path}")
+        return None
+    
+@st.cache_data
+def load_index_data(base_path, file_name):
+    file_path = os.path.join(base_path, file_name)
+    if os.path.exists(file_path):
+        return pd.Index(pd.read_csv(file_path).squeeze())
+    else:
+        st.error(f"File {file_name} not found in {base_path}")
+        return None
+
+@st.cache_data
+def create_df_music_info(df_music_source):    
+    return df_music_source[['track_id', 'name', 'artist', 'energy', 'duration_ms']]
+
+
+@st.cache_resource
+def load_pickle(base_path, file_name):
+    file_path = os.path.join(base_path, file_name)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            return pickle.load(file)
+    else:
+        st.error(f"File {file_name} not found in {base_path}")
+        return None
+
+
 st.title("Detailed recommendation process")
 
-if 'minute_processed' not in st.session_state:
-    st.session_state.minute_processed = 1
+if 'session_started' not in st.session_state:
+    st.session_state.session_started = False
+
+if 'session_minute' not in st.session_state:
+    st.session_state.session_minute = 0
 
 if 'genarated_bpms' not in st.session_state:
     st.session_state.genarated_bpms = None
 
 
-# TODO Limpiar el datast en otro archivo
-df_gym = pd.read_csv('../data/Gym Members Exercise Dataset/gym_members_exercise_tracking.csv')
-raw_df = pd.read_csv('../data/Spotify-30ksongs/spotify_songs.csv')
-df_spotify = raw_df.dropna()
+# Load data
+df_gym = load_csv(DATA_DIR, 'modified_gym_members_exercise_tracking.csv')
+df_heart_rates = load_csv(DATA_DIR, 'gym_members_heart_rates.csv')
+df_users = load_csv(DATA_DIR, 'User Listening History_modified.csv')
+df_music = load_csv(DATA_DIR, 'Music Info.csv')
 
-#Generate synthetic data
-#TODO Tenerla generada ya en otro archivo
+id_to_cluster = load_cluster_mapping(DATA_DIR, 'track_clusters.csv')
 
+user_codes = load_numpy_data(DATA_DIR, 'user_codes.npy')
+track_codes = load_numpy_data(DATA_DIR, 'track_codes.npy')
+user_uniques = load_index_data(DATA_DIR, 'user_uniques.csv')
+track_uniques = load_index_data(DATA_DIR, 'track_uniques.csv')
 
-def my_distribution(min_val, max_val, mean, std):
-    scale = max_val - min_val
-    location = min_val
-    # Mean and standard deviation of the unscaled beta distribution
-    unscaled_mean = (mean - min_val) / scale
-    unscaled_var = (std / scale) ** 2
-    # Computation of alpha and beta can be derived from mean and variance formulas
-    t = unscaled_mean / (1 - unscaled_mean)
-    beta = ((t / unscaled_var) - (t * t) - (2 * t) - 1) / ((t * t * t) + (3 * t * t) + (3 * t) + 1)
-    alpha = beta * t
-    # Not all parameters may produce a valid distribution
-    if alpha <= 0 or beta <= 0:
-        raise ValueError('Cannot create distribution for the given parameters.')
-    # Make scaled beta distribution with computed parameters
-    return scipy.stats.beta(alpha, beta, scale=scale, loc=location)
+df_music_info = create_df_music_info(df_music)
 
-def genarate_heart_rates_beta(min_val, max_val, avg, std, N):
-    my_dist = my_distribution(min_val, max_val, avg, std)
-    
-    generated_numbers = my_dist.rvs(size=N)
-    if True:  
-        x = np.linspace(min_val, max_val, 100)
-        y = my_dist.pdf(x)
-        df_distr = pd.DataFrame({'x': x, 'pdf': y})
-        df_distr.set_index('x', inplace=True)
-        
-    return generated_numbers, df_distr
+#Load interaction matrix
+interaction_matrix_user_item = load_pickle(MATRICES_DIR, 'interaction_matrix.pkl')
 
-mean_std_ratio_avg_max = 0.36578887474025085 #Obtained from doing the mean
+#Load model
+als_model = load_pickle(MODEL_DIR, 'als_model.pkl')
+
+if df_gym is None or df_heart_rates is None or df_users is None or df_music is None or id_to_cluster is None or user_codes is None or track_codes is None or user_uniques is None or track_uniques is None:
+    st.error("Error loading data files. Please check the files in the resources/data directory.")
+    st.stop()
+
+if als_model is None:
+    st.error("Error loading ALS model. Please check the model in the resources/models directory.")
+    st.stop()
+
+if interaction_matrix_user_item is None:
+    st.error("Error loading interaction matrix. Please check the matrix in the resources/matrices directory.")
+    st.stop()
 
 
-#First model - Fuzzy logic
-bpm_antecedent = ctrl.Antecedent(np.arange(30, 201, 1), 'BPM')
-bpm_variation_antecedent = ctrl.Antecedent(np.arange(-30, 31, 1), 'BPM Variation')
-intensity_consequent = ctrl.Consequent(np.arange(0, 101, 1), 'Intensity')
-
-bpm_antecedent.automf(5, names=['Very Low', 'Low', 'Medium', 'High', 'Very High'])
-bpm_variation_antecedent.automf(3, names=['Negative', 'Zero', 'Positive'])
-intensity_consequent.automf(3, names=['Low', 'Medium', 'High'])
-
-#Rules
-rule1 = ctrl.Rule(antecedent= (bpm_antecedent['Very Low'] |
-                        (bpm_antecedent['Low'] & bpm_variation_antecedent['Negative']) |
-                        (bpm_antecedent['Low'] & bpm_variation_antecedent['Zero']) |
-                        (bpm_antecedent['Medium'] & bpm_variation_antecedent['Negative'])),
-                        consequent=intensity_consequent['High'])
-rule2 = ctrl.Rule(antecedent=((bpm_antecedent['Low'] & bpm_variation_antecedent['Positive']) |
-                        (bpm_antecedent['Medium'] & bpm_variation_antecedent['Zero']) |
-                        (bpm_antecedent['High'] & bpm_variation_antecedent['Negative'])),
-                        consequent=intensity_consequent['Medium'])
-rule3 = ctrl.Rule(antecedent=((bpm_antecedent['Medium'] & bpm_variation_antecedent['Positive']) |
-                        (bpm_antecedent['High'] & bpm_variation_antecedent['Zero']) |
-                        (bpm_antecedent['High'] & bpm_variation_antecedent['Positive'])),
-                        consequent=intensity_consequent['Low'])
-
-#Controller
-intensity_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
-intensity_sim = ctrl.ControlSystemSimulation(intensity_ctrl)
-
-def calculate_intensity_fuzzy(bpm, bpm_variation, plot_consequent=False, plot_antecedent=False):
-    intensity_sim.input['BPM'] = bpm
-    intensity_sim.input['BPM Variation'] = bpm_variation
-    intensity_sim.compute()
-    if plot_consequent:
-        intensity_consequent.view(sim=intensity_sim)
-        st.pyplot(plt.gcf())
-    if plot_antecedent:
-        bpm_antecedent.view(sim=intensity_sim)
-        st.pyplot(plt.gcf())
-        bpm_variation_antecedent.view(sim=intensity_sim)
-        st.pyplot(plt.gcf())
-    return intensity_sim.output['Intensity']
-
-
-def calculate_session_moment_intensity(generated_bpms, minute, print_values=False, plot_consequent=False, plot_antecedent=False):
-    bpm_registered = generated_bpms[minute]
-    if minute == 0:
-        bpm_variation = 0
-    else:
-        last_bpm = generated_bpms[minute - 1]
-        bpm_variation = bpm_registered - last_bpm
-
-    if print_values:
-        print(f"Minute analized: {minute}")
-        print(f"BPM registered: {bpm_registered}")
-        print(f"Last BPM: {last_bpm}")
-        print(f"BPM variation: {bpm_variation}")
-    
-    intensity = calculate_intensity_fuzzy(bpm_registered, bpm_variation, plot_consequent, plot_antecedent)
-    if print_values:
-        print(f"Intensity: {intensity}")
-    return intensity
-
-option = st.selectbox("Chose a mode:", ["Manual data", "Client data"])
+option = st.selectbox("Chose a mode:", ["User data"]) #st.selectbox("Chose a mode:", ["User data", "Manual data"])
 
 # Ejecutar una acción según la opción seleccionada
-if option == "Manual data":
-    current_bpm = st.number_input('Current BPM', min_value=40, max_value=200, value=150, step=1)
-    last_bpm = st.number_input('Last measured BPM', min_value=40, max_value=200, value=150, step=1)
-    variation = current_bpm - last_bpm
+# if option == "Manual data":
+#     current_bpm = st.number_input('Current BPM', min_value=40, max_value=200, value=150, step=1)
+#     last_bpm = st.number_input('Last measured BPM', min_value=40, max_value=200, value=150, step=1)
+#     variation = current_bpm - last_bpm
     
-    if st.button('Process'):
-        calculate_intensity_fuzzy(current_bpm, variation, plot_consequent=True, plot_antecedent=True)
+#     if st.button('Process'):
+#         calculate_intensity_fuzzy(current_bpm, variation, plot_consequent=True, plot_antecedent=True)
 
-elif option == "Client data":
+if option == "User data":
     st.write("Gym Members Exercise Dataset")
-    df_clients_shown = df_gym[['Age', 'Gender', 'Weight (kg)','Height (m)', 'Session_Duration (hours)', 'Workout_Type']].copy()
-    df_clients_shown.rename(columns={'Session_Duration (hours)': 'Duration (hours)'}, inplace=True) 
-    df_clients_shown.index = df_clients_shown.index + 1
-    st.dataframe(df_clients_shown, height=300)
+    df_users_shown = df_gym[['Age', 'Gender', 'Weight (kg)','Height (m)', 'Session_Duration (hours)', 'Workout_Type']].copy()
+    df_users_shown.rename(columns={'Session_Duration (hours)': 'Duration (hours)'}, inplace=True) 
+    df_users_shown.index.name = "ID"
+    st.dataframe(df_users_shown, height=200)
 
-    client_id = st.number_input('Select a client', min_value=1, max_value=df_clients_shown.shape[0], value=1, step=1)
-    client_id = client_id - 1  # Adjust for zero-based index
-    if st.button('Select client'):
-        st.session_state.minute_processed = 1
-        min_val, max_val, avg, N = df_gym.iloc[client_id]['Resting_BPM'], df_gym.iloc[client_id]['Max_BPM'], df_gym.iloc[client_id]['Avg_BPM'], math.trunc(df_gym.iloc[client_id]['Session_Duration (hours)'] * 60)
-        std = mean_std_ratio_avg_max * (max_val - avg)
+    selected_user_id = st.number_input('Select your user ID', min_value=0, max_value=df_users_shown.shape[0] - 1, value=0, step=1)
 
-        st.session_state.genarated_bpms, df_distr = genarate_heart_rates_beta(min_val, max_val, avg, std, N)
+    if st.session_state.session_started:
+        energy_calculator = EnergyCalculator(df_gym.iloc[st.session_state.user_id], st.session_state.user_heart_rates, st.session_state.session_minute)
+        als_recommender = ALSRecommender(interaction_matrix_user_item, track_uniques, df_music_info, als_model)
+        hybrid_recommender = HybridRecommender(interaction_matrix_user_item, track_uniques, df_music_info, df_users, id_to_cluster, st.session_state.recommendations, als_recommender=als_recommender)
+        music_recommender_2_stages = MusicRecommender2Stages(energy_calculator, hybrid_recommender, st.session_state.user_id, df_music_info)
+        st.write(f"User ID: {selected_user_id}")
+        st.write("User listened songs")
+        st.dataframe(st.session_state.listened_songs)
+        st.write("Recommended Songs")
+        st.dataframe(music_recommender_2_stages.get_recommendations_info())
 
-        intensity = calculate_session_moment_intensity(st.session_state.genarated_bpms, st.session_state.minute_processed, plot_consequent=True, plot_antecedent=True)
-        st.write(f"Minute: {st.session_state.minute_processed}")
-        st.write(f"Current bpm: {int(st.session_state.genarated_bpms[st.session_state.minute_processed])}, Last bpm: {int(st.session_state.genarated_bpms[st.session_state.minute_processed - 1])}")
-        st.write(f"## Intensity: {round(intensity, 2)}")
+    if st.button('Start session'):
+        st.session_state.user_id = selected_user_id
+        st.session_state.session_minute = 0
+        st.session_state.user_heart_rates = df_heart_rates[df_heart_rates['User_ID'] == st.session_state.user_id]['Heart_Rate'].tolist()
+        st.session_state.session_started = True
 
-    if st.button('Next song'):
-        st.session_state.minute_processed += random.randint(1, 5)
-        if st.session_state.minute_processed < len(st.session_state.genarated_bpms):
-            intensity = calculate_session_moment_intensity(st.session_state.genarated_bpms, st.session_state.minute_processed, plot_consequent=True, plot_antecedent=True)
-            st.write(f"Minute: {st.session_state.minute_processed}")
-            st.write(f"Current bpm: {int(st.session_state.genarated_bpms[st.session_state.minute_processed])}, Last bpm: {int(st.session_state.genarated_bpms[st.session_state.minute_processed - 1])}")
-            st.write(f"## Intensity: {round(intensity, 2)}")
-        else:
-            st.write("Session finished")
-        st.write(f"Minute: {st.session_state.minute_processed}")
+        user_listened_songs = df_users[df_users['user_id'] == user_uniques[st.session_state.user_id]].track_id
+        st.session_state.listened_songs = df_music_info[df_music_info['track_id'].isin(user_listened_songs)]
+
+        als_recommender = ALSRecommender(interaction_matrix_user_item, track_uniques, df_music_info, als_model)
+        hybrid_recommender = HybridRecommender(interaction_matrix_user_item, track_uniques, df_music_info, df_users, id_to_cluster, als_recommender=als_recommender)
+        music_recommender_2_stages = MusicRecommender2Stages(None, hybrid_recommender, st.session_state.user_id, df_music_info)
+        music_recommender_2_stages.make_recommendations(n=100)
+        st.session_state.recommendations = music_recommender_2_stages.get_recommendations()
+        st.rerun()
+
+    if st.session_state.session_started:
+        if st.button('Next song'):
+            minute, df_recommended_song, energy, bpm_current, bpm_before = music_recommender_2_stages.recommend_song(plot_antecedent=True, plot_consequent=True)
+            st.session_state.session_minute = music_recommender_2_stages.get_session_minute()
+            st.write(f"Session minute: {minute}")
+            if df_recommended_song is None:
+                st.write("Session ended")
+                #st.stop()
+            else:
+                if minute == 0:
+                    st.write("Warm-up song")
+                else:
+                    st.write(f"Current bpm: {int(bpm_current)}, Last bpm: {int(bpm_before)}")
+                st.write(f"Energy level for next song: {energy}")
+                st.write("Recommended song")
+                st.dataframe(df_recommended_song)
+
+
+
+        if st.button('End session'):
+            st.session_state.session_started = False
+            st.session_state.session_minute = 0
+            st.session_state.genarated_bpms = None
+            st.rerun()
